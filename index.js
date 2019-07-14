4010c1bfcc929e0f12a96c2bc23eb254271be287
Ahmed Abououf <aabuauf@yahoo.com>
2019-07-14 17:18:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // add solution here
+function theBeatlesPlay(musArray,instArray){
+  var array =[]
+  for (var counter = 0 ; counter < musArray.length ;counter++){
+   array.push( musArray[counter] + " plays "+instArray[counter])
+  }
+  return array
+}
+
+function johnLennonFacts(array){
+  while (){
+    
+  }
+}
